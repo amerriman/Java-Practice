ebe710118375c35cb66fa4b3c525b69bdfb8358c
amerriman <amm434@gmail.com>
2016-04-02 10:28:13 -0600

    initial

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+
+  public static void main(String [] args){
+    System.out.println("Hello, Java!");
+    System.out.println("Hello, Java!" + 222);
+    System.out.println(222 + 14);
+  }
+}
---
+++ Ops.java
@@ -0,0 +1,17 @@
+public class Ops {
+
+  public static int sum(int x, int y){
+    return x + y;
+  }
+
+  public static int multiply(int x, int y, int z){
+    return x * y * z;
+  }
+
+  public static void main(String [] args){
+    System.out.println(sum(1,2));
+    System.out.println(multiply(3, 4, 2));
+  }
+
+
+}
